a29b9bce314b6aad22a1f3c2fce8e5248de67e18
Justin Newman <jgnewman1997@gmail.com>
2019-10-07 11:32:48 -0400

    linting

--- src/app/left-menu/FillBoard.ts
+++ src/app/left-menu/FillBoard.ts
@@ -7,65 +7,65 @@ export function fillBoard(fen: string, selectedPiece: Position) {
 
   // remove selected piece
   if (selectedPiece.number != null) {
-    document.getElementById(selectedPiece.number.toString()).style.color="black";
+    document.getElementById(selectedPiece.number.toString()).style.color = 'black';
     selectedPiece.number = null;
 }
 
 // remove all of the old pieces and make reset the colors
-for (let i = 0; i < 64 ; i++) {
-    document.getElementById(i.toString()).innerHTML="";
-    if (Math.floor(i/8)%2 == (i%8)%2) {
-        document.getElementById(i.toString()).className="black";
+  for (let i = 0; i < 64 ; i++) {
+    document.getElementById(i.toString()).innerHTML = '';
+    if (Math.floor(i / 8) % 2 === (i % 8) % 2) {
+        document.getElementById(i.toString()).className = 'black';
     } else {
-        document.getElementById(i.toString()).className="white";
+        document.getElementById(i.toString()).className = 'white';
     }
 }
 
-let fenArray = fen.split(" ")[0].split("/");
-let rankNumber = 7;
+  const fenArray = fen.split(' ')[0].split('/');
+  let rankNumber = 7;
 
-fenArray.forEach(function(element) {
-    let fileNumber=0;
+  fenArray.forEach((element) => {
+    let fileNumber = 0;
 
-    element.split("").forEach(function(otherElement) {
-      let otherElementNumber = Number(otherElement);
-        if (isNaN(otherElementNumber)) {
-            let div = document.getElementById((8*rankNumber+fileNumber).toString());
-            switch(otherElement) {
-                case "r": div.innerHTML = "&#9820;";
-                break;
-                case "n": div.innerHTML = "&#9822;";
-                break;
-                case "b": div.innerHTML = "&#9821;";
-                break;
-                case "q": div.innerHTML = "&#9819;";
-                break;
-                case "k": div.innerHTML = "&#9818;";
-                break;
-                case "p": div.innerHTML = "&#9823;";
-                break;
-                case "R": div.innerHTML = "&#9814;";
-                break;
-                case "N": div.innerHTML = "&#9816;";
-                break;
-                case "B": div.innerHTML = "&#9815;";
-                break;
-                case "Q": div.innerHTML = "&#9813;";
-                break;
-                case "K": div.innerHTML = "&#9812;";
-                break;
-                case "P": div.innerHTML = "&#9817;";
-                break;
-                default: div.innerHTML = "";
-                break;
+    element.split('').forEach((otherElement) => {
+      const otherElementNumber = Number(otherElement);
+      if (isNaN(otherElementNumber)) {
+            const div = document.getElementById((8 * rankNumber + fileNumber).toString());
+            switch (otherElement) {
+                case 'r': div.innerHTML = '&#9820;';
+                          break;
+                case 'n': div.innerHTML = '&#9822;';
+                          break;
+                case 'b': div.innerHTML = '&#9821;';
+                          break;
+                case 'q': div.innerHTML = '&#9819;';
+                          break;
+                case 'k': div.innerHTML = '&#9818;';
+                          break;
+                case 'p': div.innerHTML = '&#9823;';
+                          break;
+                case 'R': div.innerHTML = '&#9814;';
+                          break;
+                case 'N': div.innerHTML = '&#9816;';
+                          break;
+                case 'B': div.innerHTML = '&#9815;';
+                          break;
+                case 'Q': div.innerHTML = '&#9813;';
+                          break;
+                case 'K': div.innerHTML = '&#9812;';
+                          break;
+                case 'P': div.innerHTML = '&#9817;';
+                          break;
+                default: div.innerHTML = '';
+                         break;
             }
             fileNumber++;
         } else {
             fileNumber += Number(otherElement);
         }
-    })
+    });
     rankNumber--;
-})
+});
 
 
 
--- src/models/LegalMovesModel.ts
+++ src/models/LegalMovesModel.ts
@@ -2,6 +2,6 @@ import { Game } from './Game';
 import { Move } from './Move';
 
 export class LegalMovesModel {
-  game : Game;
-  legalMoves : Array<Move>;
+  game: Game;
+  legalMoves: Array<Move>;
 }
